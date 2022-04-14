15afe39892deb9031f882a2eb2f78f0224f74438
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2022-04-14 08:06:56 +0700

    ^ 🟢

---
+++ 14_Match-Beginning/script.js
@@ -0,0 +1,5 @@
+//! Caret (^) outside the classes [] to find beginning
+
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let result = calRegex.test(rickyAndCal);
