8c4004839cc8c6d4855c8b7f4bd3a188aadfe84a
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2022-04-14 08:08:05 +0700

    Shorthand [A-Za-z0-9_] 🟢

---
+++ 16_shorthand-w/script.js
@@ -0,0 +1,5 @@
+//! Count all Alphanumeric Characters
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g;
+let result = quoteSample.match(alphabetRegexV2).length;
+console.log(result);
