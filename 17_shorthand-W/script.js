9fa9e4d0edec6c8fd699487a8cc3472ec0a4a4b8
Andika Eka Kurnia <73420758+DikDns@users.noreply.github.com>
2022-04-14 08:08:23 +0700

    Shorthand [^A-Za-z0-9_] 🟢

---
+++ 17_shorthand-W/script.js
@@ -0,0 +1,5 @@
+//! match non-alphanumeric characters
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; // Change this line
+let result = quoteSample.match(nonAlphabetRegex).length;
+console.log(result);
